c2790e1ac2fe9cdb7719ce4fcf8b712b51e68fed
Andy Lindsay <andydlindsay@gmail.com>
2021-09-29 11:28:17 -0700

    create basic components for breakout

---
+++ breakout-react-router/the-router/src/components/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to our website!</h2>
+    </div>
+  );
+};
+
+export default Home;
---
+++ breakout-react-router/the-router/src/components/Products.jsx
@@ -0,0 +1,9 @@
+const Products = () => {
+  return (
+    <div>
+      <h2>Browse all of our products!</h2>
+    </div>
+  );
+};
+
+export default Products;
